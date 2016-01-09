2c68c5ac1b11b14bbaad13ada7554a8c1748a735
bec_tt <bec_tt@163.com>
2016-01-09 23:36:42 +0800

    门票service

---
+++ YttxPS/src/com/yttx/yttxps/service/ITicketService.java
@@ -0,0 +1,35 @@
+package com.yttx.yttxps.service;
+
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yttx.yttxps.model.Tticket;
+import com.yttx.yttxps.model.TticketExample;
+
+
+public interface ITicketService {
+	
+	@Transactional(readOnly = true) 
+	public int selectCountSelective(Map<String, Object> map);
+	
+	@Transactional(readOnly = true) 
+	List<Tticket> selectSelectivePage(Map<String, Object> map);
+	
+	@Transactional
+	int insert(Tticket ticket);
+	
+	@Transactional
+	int update(Tticket ticket);
+	
+	@Transactional
+	int delete(String no);
+
+	int selectMaxSeq(TticketExample example);
+	
+	List<Tticket> selectTticket(TticketExample example);
+	
+	int selectFsNo();
+}
---
+++ YttxPS/src/com/yttx/yttxps/service/imp/TicketService.java
@@ -0,0 +1,66 @@
+package com.yttx.yttxps.service.imp;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import com.yttx.yttxps.mapper.TticketMapper;
+import com.yttx.yttxps.model.Tticket;
+import com.yttx.yttxps.model.TticketExample;
+import com.yttx.yttxps.service.ITicketService;
+
+
+@Service("ticketService")
+public class TicketService implements ITicketService {
+
+	@Autowired
+	private PubService<Tticket> pubService;
+	
+	@Autowired
+	private TticketMapper<Tticket> ticketMapper;
+
+	@Override
+	public int selectCountSelective(Map<String, Object> map) {
+		return ticketMapper.selectCountSelective(map);
+	}
+
+	@Override
+	public List<Tticket> selectSelectivePage(Map<String, Object> map) {
+		return pubService.doPage(map, ticketMapper);
+	}
+
+	@Override
+	public int insert(Tticket record) {
+		return ticketMapper.insert(record);
+	}
+
+	@Override
+	public int update(Tticket record) {
+		return ticketMapper.updateByPrimaryKeySelective(record);
+	}
+
+	@Override
+	public int delete(String no) {
+		return ticketMapper.deleteByPrimaryKey(no);
+	}
+	
+	@Override
+	public int selectMaxSeq(TticketExample example) {
+		return ticketMapper.selectMaxSeqByExample(example);
+	}
+
+	@Override
+	public List<Tticket> selectTticket(TticketExample example) {
+		// TODO Auto-generated method stub
+		return ticketMapper.selectByExample(example);
+	}
+
+	@Override
+	public int selectFsNo() {
+		return ticketMapper.selectFsNo();
+	}
+	
+}
