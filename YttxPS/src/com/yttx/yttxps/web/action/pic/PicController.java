f265e1826fb45cb23ddcf7e65b2752a62468243a
lvo <fallenred@yeah.net>
2016-01-09 16:06:21 +0800

    merge

--- YttxPS/src/com/yttx/yttxps/web/action/pic/PicController.java
+++ YttxPS/src/com/yttx/yttxps/web/action/pic/PicController.java
@@ -1,6 +1,6 @@
 package com.yttx.yttxps.web.action.pic;
 
-import java.sql.SQLIntegrityConstraintViolationException;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,17 +10,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.yttx.yttxps.comm.JsonResult;
-import com.yttx.yttxps.model.Scenic;
+import com.yttx.yttxps.model.Pic;
 import com.yttx.yttxps.model.vo.PicRequest;
-import com.yttx.yttxps.model.vo.ScenicRequest;
-import com.yttx.yttxps.service.IScenicService;
+import com.yttx.yttxps.service.IPicService;
 import com.yttx.yttxps.web.action.BaseController;
 import com.yttx.yttxps.web.action.LoginController;
 
@@ -53,17 +52,18 @@ static Logger logger = LoggerFactory.getLogger(LoginController.class);
     }
 	
 	/**
-	 * 新增景区信息
-	 * @param scenic
+	 * 新图片信息
+	 * @param pic
 	 * @return
 	 */
-	@RequestMapping(value="addScenic.htm", method = RequestMethod.POST)
+	@RequestMapping(value="addPic.htm", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Object> ajaxaddScenic(Scenic scenic)
+	public Map<String, Object> ajaxaddPic(Pic pic,
+			@RequestParam(value = "file", required = true) MultipartFile file)
     {  
-		logger.debug("当前新增对象 {}", scenic);
+		logger.debug("当前新增对象 {}", pic);
 		try{
-		int ret = scenicService.insert(scenic);
+			int ret = picService.insert(pic);
 		}
 		catch(Exception e){
 			return (Map<String, Object>) JsonResult.jsonError("新增失败");
@@ -72,36 +72,17 @@ static Logger logger = LoggerFactory.getLogger(LoginController.class);
     }
 	
 	/**
-	 * 更新景区信息
+	 * 删除图片信息
 	 * @param scenic
 	 * @return
 	 */
-	@RequestMapping(value="editScenic.htm", method = RequestMethod.POST)
+	@RequestMapping(value="delPic.htm", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Object> ajaxeditScenic(Scenic scenic)
+	public Map<String, Object> ajaxdelPic(@RequestParam(value = "index") BigDecimal  index)
     {  
-		logger.debug("当前更新对象 {}", scenic);
+		logger.debug("当前删除key {}", index);
 		try{
-			int ret = scenicService.update(scenic);
-		}
-		catch(Exception e){
-			return (Map<String, Object>) JsonResult.jsonError("更新失败");
-		}
-		return (Map<String, Object>) JsonResult.jsonOk();
-    }
-	
-	/**
-	 * 删除景区信息
-	 * @param scenic
-	 * @return
-	 */
-	@RequestMapping(value="delScenic.htm", method = RequestMethod.POST)
-	@ResponseBody
-	public Map<String, Object> ajaxdelScenic(@RequestParam(value = "no") String  no)
-    {  
-		logger.debug("当前删除key {}", no);
-		try{
-			int ret = scenicService.delete(no);
+			int ret = picService.delete(index);
 		}
 		catch(Exception e){
 			return (Map<String, Object>) JsonResult.jsonError("删除失败");
