c53d0e2ea338dbf70ea57f490401b5be348f10e2
yuanhang@lucky-byte.com <yuanhang@lucky-byte.com>
2016-01-18 18:45:12 +0800

    修复地区控件：页面重复使用该控件 BUG

--- YttxPS/WebRoot/js/bus/localcity.js
+++ YttxPS/WebRoot/js/bus/localcity.js
@@ -2,31 +2,31 @@
 
 ;(function($){
 
-	
-	$.fn.localCity = function(options,fn){
-		$.fn.localCity.defaults = {  
+    
+    $.fn.localCity = function(options,fn){
+        $.fn.localCity.defaults = {  
             defaultcity:null, 
             provurl:'',
             cityurl:'',
             disturl:'',
             callback:function(){return false;}
-		};
+        };
         var opts = $.extend({}, $.fn.localCity.defaults, options);
 
         var tabs = '<div class="tabs">'+
-        			'<ul class="">'+
-        			'<li><a href="javascript:void(0);">省份</a></li>'+
-        			'<li><a href="javascript:void(0);">城市</a></li>'+
-        		    '<li><a href="javascript:void(0);">区县</a></li>'+
-        			'</ul>'+
-        			'<span class="close-x"></span>'
-        		   '</div>';
+                    '<ul class="">'+
+                    '<li><a href="javascript:void(0);">省份</a></li>'+
+                    '<li><a href="javascript:void(0);">城市</a></li>'+
+                    '<li><a href="javascript:void(0);">区县</a></li>'+
+                    '</ul>'+
+                    '<span class="close-x"></span>'
+                   '</div>';
             
         var contexts = '<div class="con">'+
-        				'<div class="list"><ul></ul></div>'+
-        				'<div class="list"><ul></ul></div>'+
-        				'<div class="list"><ul></ul></div>'+
-        			  '</div>';
+                        '<div class="list"><ul></ul></div>'+
+                        '<div class="list"><ul></ul></div>'+
+                        '<div class="list"><ul></ul></div>'+
+                      '</div>';
             
         var tags = '<li><a href="javascript:"  data-key="{key}" class="">{keyName}</a></li>';       
             
@@ -45,42 +45,42 @@
             var seldistkey = '';
                     
             if(that.find('.tabs').length <= 0)
-            	that.append(tabs);
+                that.append(tabs);
             
             if(that.find('.con').length <= 0)
-            	that.append(contexts);
+                that.append(contexts);
             
             if(opts.defaultcity != undefined && opts.defaultcity != null){
-            	var selecteds =opts.defaultcity.split('-');           	
+                var selecteds =opts.defaultcity.split('-');             
             }
             
-            function loadProv(){        	
-            	if(that.find('.list').eq(0).find('li').length == 0)            	
-            		render('0086', opts.provurl, 0);    
+            function loadProv(){            
+                if(that.find('.list').eq(0).find('li').length == 0)             
+                    render('0086', opts.provurl, 0);    
             }
             
             that.delegate('.close-x','click',function(e){
-            	$(that).removeClass('cityactive');
-			});
+                $(that).removeClass('cityactive');
+            });
             
-            function loadCity(key){          	
-            	var cityli = that.find('.list').eq(1).find('li');
-            	$(cityli).remove();
+            function loadCity(key){             
+                var cityli = that.find('.list').eq(1).find('li');
+                $(cityli).remove();
        
-            	if(that.find('.list').eq(1).find('li').length == 0)            	
-            		render(key, opts.cityurl, 1);   
-            	
-            	var countyli = that.find('.list').eq(2).find('li');
-            	$(countyli).remove();
+                if(that.find('.list').eq(1).find('li').length == 0)             
+                    render(key, opts.cityurl, 1);   
+                
+                var countyli = that.find('.list').eq(2).find('li');
+                $(countyli).remove();
             }
             
-            function loadCounty(key){             	
-            	var cityli = that.find('.list').eq(2).find('li');
-            	$(cityli).remove();
+            function loadCounty(key){               
+                var cityli = that.find('.list').eq(2).find('li');
+                $(cityli).remove();
  
-            	
-            	if(that.find('.list').eq(2).find('li').length == 0)            	
-            		render(key, opts.disturl, 2);            	
+                
+                if(that.find('.list').eq(2).find('li').length == 0)             
+                    render(key, opts.disturl, 2);               
             }
             loadProv();
             
@@ -89,7 +89,7 @@
                     return;
                 }else{
                     for(var i=0;i<sel.length;i++){
-                    	fixTags(idx, sel[i].no, sel[i].name)                       
+                        fixTags(idx, sel[i].no, sel[i].name)                       
                     }            
                 }
                 that.find('.tabs a').eq(idx).addClass('current');
@@ -97,96 +97,95 @@
             }
             
             function fixTags(idx, key, keyName){ 
-            	if(key && keyName){           		           		
-            		that.find('.list').eq(idx).find('ul').eq(0).append(tags.replace("{key}",key).replace("{keyName}",keyName));           		
-            	}
+                if(key && keyName){                                 
+                    that.find('.list').eq(idx).find('ul').eq(0).append(tags.replace("{key}",key).replace("{keyName}",keyName));                 
+                }
             }       
                
             $(this).on('click','.tabs li',function(event){
-            	
-            	var acttab = $(this);          	
-            	var tabs = $('.tabs').find('li');           	
-            	var index = tabs.index(acttab);
- console.log("tabs li index:" + index);           	                       	
-            	if(that.find('.list').eq(index).find('li').length > 0){
-            		that.find('.list').hide();
-            		that.find('.list').eq(index).show();
-            		
-            		$(tabs).find('a').removeClass('current');           	
-                	$(acttab).find('a').addClass('current');           		
-            	}
+                
+                var acttab = $(this);      
+                var tabs = that.find('.tabs').find('li');               
+                var index = tabs.index(acttab);
+ console.log("tabs li index:" + index);                                     
+                if(that.find('.list').eq(index).find('li').length > 0){
+                    that.find('.list').hide();
+                    that.find('.list').eq(index).show();
+                    
+                    $(tabs).find('a').removeClass('current');               
+                    $(acttab).find('a').addClass('current');                
+                }
             });
                        
-            $(this).on('click','.con li',function(event){            	
-          	        	
-            	var acttab = $($('.tabs').find('.current')).parents('li');          	
-            	var tabs = $('.tabs').find('li');           	
-            	var index = tabs.index(acttab);
-            	
-            	
-            	var key = $(this).find('a').eq(0).attr("data-key");
-            	var text = $(this).find('a').eq(0).text();
-            	
-            	$(this).parent().find('a').removeClass('current');                  
+            $(this).on('click','.con li',function(event){               
+                        
+                var acttab = $(that.find('.tabs').find('.current')).parents('li');              
+                var tabs = that.find('.tabs').find('li');               
+                var index = tabs.index(acttab);
+                
+                var key = $(this).find('a').eq(0).attr("data-key");
+                var text = $(this).find('a').eq(0).text();
+                
+                $(this).parent().find('a').removeClass('current');                  
                 $(this).find('a').eq(0).addClass('current');
                                   
                 index++;       
 console.log("con li index:" + index + "text:" + text);                      
                 if(index == 1){
-                	selprov = text;
-                	selprovkey = key;
-                	selcity = '';
-                	seldist = '';
-                	selcitykey = '';
-                	seldistkey = '';
-                	pager(key, text, index);
-                	$(this).parents('.list').hide();
-                	$('.tabs').find('a').removeClass('current');
+                    selprov = text;
+                    selprovkey = key;
+                    selcity = '';
+                    seldist = '';
+                    selcitykey = '';
+                    seldistkey = '';
+                    pager(key, text, index);
+                    $(this).parents('.list').hide();
+                    $('.tabs').find('a').removeClass('current');
                     if(tabs.eq(index).find('a').length>0) tabs.eq(index).find('a').addClass('current');
-                	loadCity(key);
+                    loadCity(key);
                 }else if(index == 2){
-                	selcity = text;
-                	selcitykey = key;
-                	seldist = '';
-                	seldistkey = '';
-                	pager(key, text, index);
-                	$(this).parents('.list').hide();  
-                	$('.tabs').find('a').removeClass('current');
+                    selcity = text;
+                    selcitykey = key;
+                    seldist = '';
+                    seldistkey = '';
+                    pager(key, text, index);
+                    $(this).parents('.list').hide();  
+                    $('.tabs').find('a').removeClass('current');
                     if(tabs.eq(index).find('a').length>0) tabs.eq(index).find('a').addClass('current');
-                	loadCounty(key);
+                    loadCounty(key);
                 }else{
-                	seldist = text;
-                	seldistkey = key;
-                	 pager(key, text, index);
-                	$(that).removeClass('cityactive');
+                    seldist = text;
+                    seldistkey = key;
+                     pager(key, text, index);
+                    $(that).removeClass('cityactive');
                 }
             });
             
 
             function pager(key, value, index){ 
-            	if(typeof opts.callback != 'undefined'){ 
-            		
-            		var fullname = '';
-            		var fullkey = '';
-            		
-            		if(selprovkey != ''){
-            			fullname = selprov;
-            			fullkey = selprovkey;
-            		}
-            		
-            		if(selcity != ''){
-            			fullname += ('-'+selcity);
-            			fullkey += ('-'+selcitykey);
-            		}
-            		
-            		if(seldist != ''){
-            			fullname += ('-'+seldist);
-            			fullkey += ('-'+seldistkey);
-            		}
-            		
-                	 var propagation = opts.callback(index, key, value, fullkey, fullname);	
-                	 return propagation;
-                }	
+                if(typeof opts.callback != 'undefined'){ 
+                    
+                    var fullname = '';
+                    var fullkey = '';
+                    
+                    if(selprovkey != ''){
+                        fullname = selprov;
+                        fullkey = selprovkey;
+                    }
+                    
+                    if(selcity != ''){
+                        fullname += ('-'+selcity);
+                        fullkey += ('-'+selcitykey);
+                    }
+                    
+                    if(seldist != ''){
+                        fullname += ('-'+seldist);
+                        fullkey += ('-'+seldistkey);
+                    }
+                    
+                     var propagation = opts.callback(index, key, value, fullkey, fullname); 
+                     return propagation;
+                }   
             }
             
             
@@ -198,12 +197,12 @@ console.log("con li index:" + index + "text:" + text);
                     data:postParam,    
                     cache:false,    
                     dataType:'json',    
-                    success:function(res){	
-                    	if(res.succflag==0){
-                    		init(idx, res.data);
-                    	}                             			    
+                    success:function(res){  
+                        if(res.succflag==0){
+                            init(idx, res.data);
+                        }                                           
                     }
-				});                
+                });                
             }  
         }) 
     }
