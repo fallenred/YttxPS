6223ba46172786220e3237019d53e51a459bf908
bec_tt <bec_tt@163.com>
2016-01-25 22:40:34 +0800

    新增订单service

---
+++ YttxPS/src/com/yttx/yttxps/service/imp/OrderlistService.java
@@ -0,0 +1,56 @@
+package com.yttx.yttxps.service.imp;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yttx.yttxps.mapper.TOrderlistMapper;
+import com.yttx.yttxps.model.TOrderlistExample;
+import com.yttx.yttxps.model.TOrderlistWithBLOBs;
+import com.yttx.yttxps.service.IOrderlistService;
+import com.yttx.yttxps.service.IPubService;
+
+
+@Service("orderlistService")
+public class OrderlistService implements IOrderlistService {
+
+	@Autowired
+	private IPubService<TOrderlistWithBLOBs> pubService;
+	
+	@Autowired
+	private TOrderlistMapper<TOrderlistWithBLOBs> orderlistMapper;
+
+	@Override
+	public int selectCountSelective(Map<String, Object> map) {
+		return orderlistMapper.selectCountSelective(map);
+	}
+
+	@Override
+	public List<TOrderlistWithBLOBs> selectSelectivePage(Map<String, Object> map) {
+		return pubService.doPage(map, orderlistMapper);
+	}
+
+	@Override
+	public int insert(TOrderlistWithBLOBs record) {
+		return orderlistMapper.insert(record);
+	}
+
+	@Override
+	public int update(TOrderlistWithBLOBs record) {
+		return orderlistMapper.updateByPrimaryKeySelective(record);
+	}
+
+	@Override
+	public int delete(String no) {
+		return orderlistMapper.deleteByPrimaryKey(no);
+	}
+
+	@Override
+	public List<TOrderlistWithBLOBs> selectTOrderlist(TOrderlistExample example) {
+		// TODO Auto-RouteArrangeerated method stub
+		return orderlistMapper.selectByExampleWithBLOBs(example);
+	}
+	
+}
