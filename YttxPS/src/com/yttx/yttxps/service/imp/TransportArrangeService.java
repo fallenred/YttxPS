a30a5a629d9616684abb7c66ea97cb53f6b1c533
bec_tt <bec_tt@163.com>
2016-02-03 00:41:16 +0800

    修改车型线路service

--- YttxPS/src/com/yttx/yttxps/service/imp/TransportArrangeService.java
+++ YttxPS/src/com/yttx/yttxps/service/imp/TransportArrangeService.java
@@ -5,8 +5,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.yttx.yttxps.mapper.TCCPriceMapper;
 import com.yttx.yttxps.mapper.TtransportArrangeMapper;
+import com.yttx.yttxps.model.TCCPrice;
+import com.yttx.yttxps.model.TCCPriceExample;
+import com.yttx.yttxps.model.TCCPriceExample.Criteria;
 import com.yttx.yttxps.model.TtransportArrange;
 import com.yttx.yttxps.model.TtransportArrangeExample;
 import com.yttx.yttxps.model.TtransportArrangeKey;
@@ -22,6 +27,9 @@ public class TransportArrangeService implements ITransportArrangeService {
 	
 	@Autowired
 	private TtransportArrangeMapper<TtransportArrange> transportArrangeMapper;
+	
+	@Autowired
+	private TCCPriceMapper priceMapper;
 
 	@Override
 	public int selectCountSelective(Map<String, Object> map) {
@@ -34,24 +42,61 @@ public class TransportArrangeService implements ITransportArrangeService {
 	}
 
 	@Override
-	public int insert(TtransportArrange record) {
-		return transportArrangeMapper.insert(record);
+	@Transactional(rollbackFor=Exception.class)
+	public void insert(TtransportArrange record) {
+		transportArrangeMapper.insert(record);
+		TCCPrice price = new TCCPrice();
+		price.setFtStartdate(record.getFtStartdate());
+		price.setFsRestype("cx");
+		price.setFsResno(record.getFsNo());
+		price.setFsCcno("000023");
+		price.setFdPrice(record.getFdPrice());
+		priceMapper.insert(price);
+		
 	}
 
 	@Override
-	public int update(TtransportArrange record) {
-		return transportArrangeMapper.updateByPrimaryKeySelective(record);
+	public void update(TtransportArrange record) {
+		TtransportArrangeExample example = new TtransportArrangeExample();
+		TtransportArrangeExample.Criteria criteria = example.createCriteria();
+		criteria.andFsNoEqualTo(record.getFsNo());
+		transportArrangeMapper.updateByExample(record, example);
+		TCCPriceExample priceExample = new TCCPriceExample();
+		Criteria criteria1 = priceExample.createCriteria();
+		criteria1.andFsResnoEqualTo(record.getFsNo());
+		criteria1.andFsRestypeEqualTo("cx");
+		criteria1.andFsCcnoEqualTo("000023");
+		TCCPrice price = new TCCPrice();
+		price.setFsRestype("cx");
+		price.setFsCcno("000023");
+		price.setFdPrice(record.getFdPrice());
+		priceMapper.updateByExampleSelective(price, priceExample);
 	}
 
 	@Override
-	public int delete(TtransportArrangeKey key) {
-		return transportArrangeMapper.deleteByPrimaryKey(key);
+	public int deleteByNo(String no) {
+		TCCPriceExample priceExample = new TCCPriceExample();
+		Criteria criteria1 = priceExample.createCriteria();
+		criteria1.andFsResnoEqualTo(no);
+		criteria1.andFsRestypeEqualTo("cx");
+		criteria1.andFsCcnoEqualTo("000023");
+		priceMapper.deleteByExample(priceExample);
+		TtransportArrangeExample example = new TtransportArrangeExample();
+		TtransportArrangeExample.Criteria criteria= example.createCriteria();
+		criteria.andFsNoEqualTo(no);
+		return transportArrangeMapper.deleteByExample(example);
+		
 	}
 	
 	@Override
-	public List<TtransportArrange> selectTtransportArrange(TtransportArrangeExample example) {
+	public List<TtransportArrangeKey> selectTtransportArrange(TtransportArrangeExample example) {
 		// TODO Auto-generated method stub
 		return transportArrangeMapper.selectByExample(example);
+	}
+	
+	@Override
+	public int selectFsNo() {
+		return transportArrangeMapper.selectFsNo();
 	}
 	
 }
