d9910396db26721caeea7a0f4dffe59b1954572e
183395582 <htao10210>
2016-02-02 11:25:05 +0800

    新增导游和购物service

---
+++ YttxPS/src/com/yttx/yttxps/service/imp/GuideService.java
@@ -0,0 +1,54 @@
+package com.yttx.yttxps.service.imp;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yttx.yttxps.mapper.TguideMapper;
+import com.yttx.yttxps.model.Tguide;
+import com.yttx.yttxps.service.IGuideService;
+import com.yttx.yttxps.service.IPubService;
+import com.yttx.yttxps.service.IShopService;
+
+
+@Service("guideService")
+public class GuideService implements IGuideService {
+
+	@Autowired
+	private IPubService<Tguide> pubService;
+	
+	@Autowired
+	private TguideMapper guideMapper;
+
+	@Override
+	public int selectCountSelective(Map<String, Object> map) {
+		return guideMapper.selectCountSelective(map);
+	}
+
+	@Override
+	public List<Tguide> selectSelectivePage(Map<String, Object> map) {
+		return pubService.doPage(map, guideMapper);
+	}
+
+	@Override
+	public int insert(Tguide record) {
+		return guideMapper.insert(record);
+	}
+
+	@Override
+	public int update(Tguide record) {
+		return guideMapper.updateByPrimaryKeySelective(record);
+	}
+
+	@Override
+	public int delete(String no) {
+		return guideMapper.deleteByPrimaryKey(no);
+	}
+
+
+
+
+
+}
