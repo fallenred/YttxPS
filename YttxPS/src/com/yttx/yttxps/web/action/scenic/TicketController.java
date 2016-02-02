5b6aae8617fa4971a3ca30fe97cda31fe803ec29
bec_tt <bec_tt@163.com>
2016-02-03 00:48:12 +0800

    修改门票controller

--- YttxPS/src/com/yttx/yttxps/web/action/scenic/TicketController.java
+++ YttxPS/src/com/yttx/yttxps/web/action/scenic/TicketController.java
@@ -1,5 +1,6 @@
 package com.yttx.yttxps.web.action.scenic;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,24 +16,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yttx.yttxps.comm.JsonResult;
+import com.yttx.yttxps.model.TCCPrice;
 import com.yttx.yttxps.model.Tticket;
+import com.yttx.yttxps.model.TticketExample;
+import com.yttx.yttxps.model.vo.GenRequest;
 import com.yttx.yttxps.model.vo.TicketRequest;
 import com.yttx.yttxps.service.ITicketService;
 import com.yttx.yttxps.web.action.BaseController;
-import com.yttx.yttxps.web.action.LoginController;
 
 @Controller
 @Scope("prototype")
 @RequestMapping("ticket/")
 public class TicketController extends BaseController {
 	
-static Logger logger = LoggerFactory.getLogger(LoginController.class);
+static Logger logger = LoggerFactory.getLogger(TicketController.class);
 	
 	@Autowired
 	private ITicketService ticketService;
 	
 	/**
-	 * 分页查询景区信息
+	 * 分页查询门票信息
 	 * @param req
 	 * @return
 	 */
@@ -50,7 +53,40 @@ static Logger logger = LoggerFactory.getLogger(LoginController.class);
     }
 	
 	/**
-	 * 新增景区信息
+	 * 分页查询门票价格信息
+	 * @param req
+	 * @return
+	 */
+	@RequestMapping(value="findTicketPrice.htm", method = RequestMethod.POST)
+	@ResponseBody
+	public Object ajaxfindTicketPrice(TicketRequest req)
+    {  
+		logger.debug("当前查询条件 {}", req.getTicket());
+		Map<String, Object> map = new HashMap<String, Object>();
+		req.copyPage(map);
+		req.copyTicket(map);
+		List<Tticket> list = ticketService.selectTicketPricePage(map);
+		map.put("rows", list);
+		return map;
+    }
+	
+	/**
+	 * 获取门票列表
+	 * @param req
+	 * @return
+	 */
+	@RequestMapping(value="selectTicket.htm", method = RequestMethod.GET)
+	@ResponseBody
+	public Object ajaxSelectGen(GenRequest req)
+    {  
+		logger.debug("当前查询条件 {}", req.getGen());
+		TticketExample example = new TticketExample();
+		List<Tticket> list = ticketService.selectTticket(example);
+		return list;
+    }
+	
+	/**
+	 * 新增门票信息
 	 * @param Ticket
 	 * @return
 	 */
@@ -63,13 +99,34 @@ static Logger logger = LoggerFactory.getLogger(LoginController.class);
 			ticketService.insert(ticket);
 		}
 		catch(Exception e){
+			logger.error(e.getMessage());
 			return (Map<String, Object>) JsonResult.jsonError("新增失败");
 		}
 		return (Map<String, Object>) JsonResult.jsonOk();
     }
 	
 	/**
-	 * 更新景区信息
+	 * 新增门票信息
+	 * @param Ticket
+	 * @return
+	 */
+	@RequestMapping(value="addTicketPrice.htm", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> ajaxaddTicketPrice(Tticket ticket)
+    {  
+		logger.debug("当前新增对象 {}", ticket);
+		try{
+			ticketService.insertTicketPrice(ticket);
+		}
+		catch(Exception e){
+			logger.error(e.getMessage());
+			return (Map<String, Object>) JsonResult.jsonError("新增失败");
+		}
+		return (Map<String, Object>) JsonResult.jsonOk();
+    }
+	
+	/**
+	 * 更新门票信息
 	 * @param Ticket
 	 * @return
 	 */
@@ -82,25 +139,67 @@ static Logger logger = LoggerFactory.getLogger(LoginController.class);
 			ticketService.update(ticket);
 		}
 		catch(Exception e){
+			logger.error(e.getMessage());
 			return (Map<String, Object>) JsonResult.jsonError("更新失败");
 		}
 		return (Map<String, Object>) JsonResult.jsonOk();
     }
 	
 	/**
-	 * 删除景区信息
+	 * 更新门票信息
+	 * @param Ticket
+	 * @return
+	 */
+	@RequestMapping(value="editTicketPrice.htm", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> ajaxeditTicketPrice(Tticket ticket)
+    {  
+		logger.debug("当前更新对象 {}", ticket);
+		try{
+			ticketService.updateTicketPrice(ticket);
+		}
+		catch(Exception e){
+			logger.error(e.getMessage());
+			return (Map<String, Object>) JsonResult.jsonError("更新失败");
+		}
+		return (Map<String, Object>) JsonResult.jsonOk();
+    }
+	
+	/**
+	 * 删除门票信息
 	 * @param Ticket
 	 * @return
 	 */
 	@RequestMapping(value="delTicket.htm", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Object> ajaxdelTicket(@RequestParam(value = "no") String  no)
+	public Map<String, Object> ajaxdelTicket(@RequestParam(value = "no") String  no, Date ftStartdate, Date ftEnddate)
     {  
 		logger.debug("当前删除key {}", no);
 		try{
 			ticketService.delete(no);
 		}
 		catch(Exception e){
+			logger.error(e.getMessage());
+			return (Map<String, Object>) JsonResult.jsonError("删除失败");
+		}
+		return (Map<String, Object>) JsonResult.jsonOk();
+    }
+	
+	/**
+	 * 删除门票信息
+	 * @param Ticket
+	 * @return
+	 */
+	@RequestMapping(value="delTicketPrice.htm", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> ajaxdelTicketPrice(TCCPrice tccPrice)
+    {  
+		logger.debug("当前删除key {}", tccPrice);
+		try{
+			ticketService.deleteTicketPrice(tccPrice);
+		}
+		catch(Exception e){
+			logger.error(e.getMessage());
 			return (Map<String, Object>) JsonResult.jsonError("删除失败");
 		}
 		return (Map<String, Object>) JsonResult.jsonOk();
