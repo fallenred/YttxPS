6a8c08812544d09654875296549f1ee6ea5a3d70
sunflower123456 <csp1008610086>
2016-02-19 14:48:06 +0800

    还原dept,oper相关文件
    还原dept,oper相关文件

--- YttxPS/src/com/yttx/yttxps/mapper/SysOperMapper.java
+++ YttxPS/src/com/yttx/yttxps/mapper/SysOperMapper.java
@@ -1,16 +1,26 @@
 package com.yttx.yttxps.mapper;
 
+import java.util.Map;
+
 import com.yttx.yttxps.model.SysOper;
 
-public interface SysOperMapper {
-	
+public interface SysOperMapper extends IBaseMapper<SysOper>{
 	
 	public SysOper findById(String  sysOperId);
 	
 	public int  insert(SysOper sysOper);
 	
 	public int  update(SysOper sysOper);
-	
 
+	public void updateOperStatByDepNo(SysOper oper);
+    
+    /**
+     * 更新用户信息
+     */
+    public boolean updateOperByOperId(Map<String, Object> map);
 
+    /**
+     * 删除一个用户
+     */
+	public void deleteOperByOperId(String sysOperId);
 }
