01aa0cba00ca01ae82a971b22751740f59e9442d
bec_tt <bec_tt@163.com>
2016-02-28 23:36:07 +0800

    修改订单批次

--- YttxPS/src/com/yttx/yttxps/service/imp/OrderCustomService.java
+++ YttxPS/src/com/yttx/yttxps/service/imp/OrderCustomService.java
@@ -5,18 +5,21 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
-import org.joda.time.Days;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.yttx.yttxps.mapper.TOrderCustomMapper;
+import com.yttx.yttxps.mapper.TOrderlistMapper;
+import com.yttx.yttxps.model.TOrderCustom;
 import com.yttx.yttxps.model.TOrderCustomExample;
+import com.yttx.yttxps.model.TOrderCustomExample.Criteria;
 import com.yttx.yttxps.model.TOrderCustomWithBLOBs;
+import com.yttx.yttxps.model.TOrderlistWithBLOBs;
 import com.yttx.yttxps.service.IOrderCustomService;
 import com.yttx.yttxps.service.IPubService;
 import com.yttx.yttxps.xml.ResScheduleXMLConverter;
 import com.yttx.yttxps.xml.bean.Body;
-import com.yttx.yttxps.xml.bean.Cclist;
 import com.yttx.yttxps.xml.bean.Daylist;
 import com.yttx.yttxps.xml.bean.Reslist;
 import com.yttx.yttxps.xml.bean.Root;
@@ -30,6 +33,9 @@ public class OrderCustomService implements IOrderCustomService {
 	
 	@Autowired
 	private TOrderCustomMapper<TOrderCustomWithBLOBs> orderCustomMapper;
+	
+	@Autowired
+	private TOrderlistMapper<TOrderlistWithBLOBs> orderlistMapper;
 
 	@Override
 	public int selectCountSelective(Map<String, Object> map) {
@@ -47,9 +53,11 @@ public class OrderCustomService implements IOrderCustomService {
 	}
 
 	@Override
+	@Transactional(rollbackFor=Exception.class)
 	public int update(TOrderCustomWithBLOBs record) throws Exception {
 		Body body = record.getBody();
 		String fcResSnapshot = "";
+		//设置餐厅数量
 		if (body != null) {
 			for (Daylist daylist : body.getDaylist()) {
 				for(Reslist reslist : daylist.getReslist()){
@@ -63,9 +71,29 @@ public class OrderCustomService implements IOrderCustomService {
 			Root root = new Root(record.getBody());
 			fcResSnapshot = ResScheduleXMLConverter.toXml("http://www.yttx.com/", root);
 		}
-		//System.out.println(fcResSnapshot);
 		record.setFcRessnapshot(fcResSnapshot);
-		return orderCustomMapper.updateByPrimaryKeySelective(record);
+		//更新批次成功后修改订单预估金额，以及将其余批次精确资源为空的设置为本批次资源
+		int num = 0;
+		if ((num = orderCustomMapper.updateByPrimaryKeySelective(record)) == 1){
+			TOrderCustomExample example = new TOrderCustomExample();
+			Criteria criteria = example.createCriteria();
+			criteria.andFsOrderIdEqualTo(record.getFsOrderId());
+			List<TOrderCustomWithBLOBs> list = orderCustomMapper.selectByExample(example);
+			BigDecimal totalfee = BigDecimal.ZERO;
+			for (TOrderCustomWithBLOBs orderCustom : list) {
+				if(StringUtils.isEmpty(orderCustom.getFcRessnapshot())){
+					orderCustom.setFcRessnapshot(record.getFcRessnapshot());
+					totalfee.add(record.getFdAmt());
+					orderCustomMapper.updateByPrimaryKey(orderCustom);
+				}
+				totalfee.add(orderCustom.getFdAmt());
+			}
+			TOrderlistWithBLOBs orderlist = new TOrderlistWithBLOBs();
+			orderlist.setFsNo(record.getFsOrderId());
+			orderlist.setFdTotalfee(totalfee);
+			orderlistMapper.updateByPrimaryKeySelective(orderlist);
+		}
+		return num;
 	}
 
 	@Override
