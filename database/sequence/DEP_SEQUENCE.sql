01e7a2a90ecabd032dad71c61d567526e478b33c
wangkai <kaige9at@hotmail.com>
2016-02-29 11:23:41 +0800

    添加序列创建脚本

---
+++ database/sequence/DEP_SEQUENCE.sql
@@ -0,0 +1,9 @@
+-- Create sequence 
+create sequence DEP_SEQUENCE
+minvalue 1
+maxvalue 9999999999
+start with 71
+increment by 1
+cache 10;
+
+exit;
---
+++ database/sequence/FSTATEMENT_SEQUENCE.sql
@@ -0,0 +1,9 @@
+-- Create sequence 
+create sequence FSTATEMENT_SEQUENCE
+minvalue 1
+maxvalue 999999999
+start with 7
+increment by 1
+nocache;
+
+exit;
