d2f254f7fa783ba6d5c9d6e6c6b0f65badf4b449
bec_tt <bec_tt@163.com>
2016-03-02 13:25:55 +0800

    修复bug

--- YttxPS/src/com/yttx/yttxps/service/imp/OrderCustomService.java
+++ YttxPS/src/com/yttx/yttxps/service/imp/OrderCustomService.java
@@ -1,6 +1,7 @@
 package com.yttx.yttxps.service.imp;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +19,7 @@ import com.yttx.yttxps.service.IOrderCustomService;
 import com.yttx.yttxps.service.IPubService;
 import com.yttx.yttxps.xml.ResScheduleXMLConverter;
 import com.yttx.yttxps.xml.bean.Body;
+import com.yttx.yttxps.xml.bean.Cclist;
 import com.yttx.yttxps.xml.bean.Daylist;
 import com.yttx.yttxps.xml.bean.Reslist;
 import com.yttx.yttxps.xml.bean.Root;
@@ -55,18 +57,33 @@ public class OrderCustomService implements IOrderCustomService {
 		Body body = record.getBody();
 		String fcResSnapshot = "";
 		//设置餐厅数量
-		if (body != null) {
-			for (Daylist daylist : body.getDaylist()) {
-				for(Reslist reslist : daylist.getReslist()){
-					for (int i = 0; i < reslist.getCclist().size(); i++) {
-						if ("ct".equals(reslist.getRestype())) {
-							reslist.getCclist().get(i).setUsernum(reslist.getCclist().get(0).getUsernum());
+		try{
+			if (body != null) {
+				for (Daylist daylist : body.getDaylist()) {
+					if(daylist.getReslist() == null) continue;
+					List<Reslist> resList = new ArrayList<Reslist>();
+					for(Reslist reslist : daylist.getReslist()){
+						if (reslist == null || reslist.getCclist() == null) continue;
+						resList.add(reslist);
+						String usernum = reslist.getCclist().get(0).getUsernum();
+						List<Cclist> ccList = new ArrayList<Cclist>();
+						for (Cclist cclist : reslist.getCclist()) {
+							cclist.setUsernum(usernum);
+							if (cclist.getPrice() == null){
+								continue;
+							}
+							ccList.add(cclist);
 						}
+						reslist.setCclist(ccList);
 					}
+					daylist.setReslist(resList);
 				}
+				Root root = new Root(record.getBody());
+				fcResSnapshot = ResScheduleXMLConverter.toXml("http://www.yttx.com/", root);
 			}
-			Root root = new Root(record.getBody());
-			fcResSnapshot = ResScheduleXMLConverter.toXml("http://www.yttx.com/", root);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
 		}
 		record.setFcRessnapshot(fcResSnapshot);
 		//更新批次成功后修改订单预估金额，以及将其余批次精确资源为空的设置为本批次资源
@@ -83,7 +100,7 @@ public class OrderCustomService implements IOrderCustomService {
 					totalfee.add(record.getFdAmt());
 					orderCustomMapper.updateByPrimaryKey(orderCustom);
 				}
-				totalfee.add(orderCustom.getFdAmt());
+				totalfee = totalfee.add(orderCustom.getFdAmt());
 			}
 			TOrderlistWithBLOBs orderlist = new TOrderlistWithBLOBs();
 			orderlist.setFsNo(record.getFsOrderId());
