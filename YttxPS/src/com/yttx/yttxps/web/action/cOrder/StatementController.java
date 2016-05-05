1e8eaaeab34c7d4a63a0b903abfdd43fa5b5eb8a
bec_tt <bec_tt@163.com>
2016-05-05 18:23:46 +0800

    修复bug

--- YttxPS/src/com/yttx/yttxps/web/action/cOrder/StatementController.java
+++ YttxPS/src/com/yttx/yttxps/web/action/cOrder/StatementController.java
@@ -36,7 +36,6 @@ import com.yttx.yttxps.service.IFStatementService;
 import com.yttx.yttxps.service.IMsgService;
 import com.yttx.yttxps.web.action.BaseController;
 import com.yttx.yttxps.xml.ResScheduleXMLConverter;
-import com.yttx.yttxps.xml.bean.closeList.ResultList;
 import com.yttx.yttxps.xml.bean.closeList.Root;
 import com.yttx.yttxps.xml.bean.closeList.Shop;
 
