3758a2a47222342ee2ccd11ae5e0e2ffd043b03d
yyqady <yyqady@gmail.com>
2016-05-11 14:57:00 +0800

    结算单生成字段修改

--- YttxPS/src/com/yttx/yttxps/service/imp/CloselistService.java
+++ YttxPS/src/com/yttx/yttxps/service/imp/CloselistService.java
@@ -1,6 +1,7 @@
 package com.yttx.yttxps.service.imp;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -11,14 +12,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.yttx.yttxps.mapper.DictMapper;
+import com.yttx.yttxps.mapper.RoomMapper;
 import com.yttx.yttxps.mapper.TCloselistMapper;
+import com.yttx.yttxps.mapper.TRestaurantMapper;
+import com.yttx.yttxps.mapper.TticketMapper;
 import com.yttx.yttxps.xml.bean.closeList.Car;
 import com.yttx.yttxps.xml.bean.closeList.CostDetails;
 import com.yttx.yttxps.xml.bean.closeList.Other;
+import com.yttx.yttxps.model.Dict;
+import com.yttx.yttxps.model.DictKey;
+import com.yttx.yttxps.model.Room;
 import com.yttx.yttxps.model.TCloselist;
 import com.yttx.yttxps.model.TCloselistExample;
 import com.yttx.yttxps.model.TOrderCustomWithBLOBs;
 import com.yttx.yttxps.model.TOrderlistWithBLOBs;
+import com.yttx.yttxps.model.TRestaurant;
+import com.yttx.yttxps.model.Tticket;
 import com.yttx.yttxps.service.ICloselistService;
 import com.yttx.yttxps.service.IPubService;
 import com.yttx.yttxps.xml.ResScheduleXMLConverter;
@@ -37,6 +47,18 @@ public class CloselistService implements ICloselistService {
 
 	@Autowired
 	private IPubService<TCloselist> pubService;
+	
+	@Autowired
+	private RoomMapper<Room> roomMapper;
+	
+	@Autowired
+	private TRestaurantMapper<Restaurant> restaurantMapper;
+	
+	@Autowired
+	private TticketMapper<Tticket> ticketMapper;
+	
+	@Autowired
+	private DictMapper dictMapper;
 	
 	@Autowired
 	private TCloselistMapper<TCloselist> closelistMapper;
@@ -115,9 +137,9 @@ public class CloselistService implements ICloselistService {
 		other.setReslist(otherReslist);
 		
 		com.yttx.yttxps.xml.bean.closeList.Reslist insuranceResList = new com.yttx.yttxps.xml.bean.closeList.Reslist();
-		insuranceResList.setTypeno("01");//待定字段
+		insuranceResList.setTypeno("01");
 		insuranceResList.setName("保险");
-		insuranceResList.setNumber(orderList.getFiVisitornum() == null ? "0" : orderList.getFdInsuerprice().toString());
+		insuranceResList.setNumber(orderList.getFiVisitornum() == null ? "0" : orderList.getFdInsuerprice().toString());//待订单修改
 		insuranceResList.setTotalprice(orderList.getFdInsuerprice() == null ? "0" : orderList.getFdInsuerprice().toString());//待订单修改
 		otherReslist.add(insuranceResList);
 		
@@ -137,7 +159,7 @@ public class CloselistService implements ICloselistService {
 					} else if(reslist.getRestype().equals("dy")) {   //导游
 						com.yttx.yttxps.xml.bean.closeList.Reslist dyResList = new com.yttx.yttxps.xml.bean.closeList.Reslist();
 						Cclist dylist = reslist.getCclist().get(0);
-						dyResList.setTypeno("02");//待定字段
+						dyResList.setTypeno("02");
 						dyResList.setName(reslist.getResname());
 						dyResList.setUnitprice(dylist.getPrice().toString());
 						dyResList.setNumber(dylist.getUsernum());
@@ -151,14 +173,16 @@ public class CloselistService implements ICloselistService {
 				Calendar cal = Calendar.getInstance();
 				cal.setTime(orderList.getFtStartdate());
 				cal.add(Calendar.DATE,Integer.parseInt(daylist.getDayflag()));
-				Date startDate = cal.getTime();
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+				String startDate = formatter.format(cal.getTime());
 				for (Reslist reslist : daylist.getReslist()) {
 					if(reslist.getRestype().equals("mp")) {   //门票
 						com.yttx.yttxps.xml.bean.closeList.Reslist mpResList = new com.yttx.yttxps.xml.bean.closeList.Reslist();
 						Cclist mplist = reslist.getCclist().get(0);
 						mpResList.setResno(reslist.getResno());
 						mpResList.setName(reslist.getResname());
-						mpResList.setType(mplist.getCcno());
+						Tticket tmp = ticketMapper.selectByPrimaryKey(reslist.getResno());
+						mpResList.setType(tmp == null ? "" : tmp.getFsType());
 						mpResList.setTypename(mplist.getCcname());
 						mpResList.setUnitprice(mplist.getPrice().toString());
 						mpResList.setNumber(mplist.getUsernum());
@@ -170,8 +194,9 @@ public class CloselistService implements ICloselistService {
 						Cclist ctlist = reslist.getCclist().get(0);
 						ctResList.setResno(reslist.getResno());
 						ctResList.setName(reslist.getResname());
-						ctResList.setType("");//待订单修改
-						ctResList.setTime(startDate.toString());
+						TRestaurant tmp = restaurantMapper.selectByPrimaryKey(reslist.getResno());
+						ctResList.setType(tmp == null ? "" : tmp.getSpecial());
+						ctResList.setTime(startDate);
 						ctResList.setUnitprice(ctlist.getPrice().toString());
 						ctResList.setNumber(ctlist.getUsernum());
 						ctResList.setTotalprice((ctlist.getPrice().multiply(new BigDecimal(ctlist.getUsernum()))).toString());
@@ -180,11 +205,19 @@ public class CloselistService implements ICloselistService {
 					} else if(reslist.getRestype().equals("bg")) {   //房
 						com.yttx.yttxps.xml.bean.closeList.Reslist bgResList = new com.yttx.yttxps.xml.bean.closeList.Reslist();
 						Cclist bglist = reslist.getCclist().get(0);
-						bgResList.setAccomno(reslist.getResno());
+						Room tmp = roomMapper.selectByPrimaryKey(reslist.getResno());
+						bgResList.setAccomno(tmp == null ? "" : tmp.getFsAccomno());
 						bgResList.setName(reslist.getResname());
-						bgResList.setRoomtypeno(null);//待定字段
-						bgResList.setRoomtype(null);//待定字段
-						bgResList.setTime(startDate.toString());
+						Dict dict = null;
+						if(tmp != null) {
+							DictKey dictKey = new DictKey();
+							dictKey.setFsDictno(tmp.getFsType());
+							dictKey.setFsParentno("fx");
+							dict = dictMapper.selectByPrimaryKey(dictKey);
+						}
+						bgResList.setRoomtypeno(dict == null ? "" : dict.getFsDictno());
+						bgResList.setRoomtype(dict == null ? "" : dict.getFsDictname());
+						bgResList.setTime(startDate);
 						bgResList.setUnitprice(bglist.getPrice().toString());
 						bgResList.setNumber(bglist.getUsernum());
 						bgResList.setTotalprice((bglist.getPrice().multiply(new BigDecimal(bglist.getUsernum()))).toString());
